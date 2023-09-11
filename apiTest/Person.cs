e08a54e8fbcb77984619e969be7eda8eb017980e
victormatta <vmatta0708@gmail.com>
2023-09-11 18:46:39 -0300

    Adicionar arquivos de projeto.

---
+++ apiTest/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace apiTest
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ apiTest/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apiTest
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PersonController : ControllerBase
+        {
+            [HttpGet]
+            public ActionResult<Person> Get()
+            {
+                var person = new Person
+                {
+                    Name = "Victor",
+                    Description = "Victor is a Software Developer and he uses some programming languages such as C#, JavaScript (node), php and flutter"
+                };
+
+                return person;
+            }
+        }
+    
+}
